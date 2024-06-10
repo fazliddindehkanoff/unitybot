5d55c7fbc019a9a73eab051b5c0efca5cd378c0d
FazliddinDehkanoff <fazliddindehkanoff@yandex.com>
2024-06-10 19:04:36 +0500

    celery has been configured

--- tasks.py
+++ tasks.py
@@ -1,5 +1,10 @@
+import pandas
+
+from datetime import datetime, timedelta
 from celery import Celery, shared_task
-from loader import groups
+from celery.schedules import schedule
+from loader import groups, db, bot
+from utils.extra_datas import invite_to_test
 
 app = Celery(
     "my_bot",
@@ -10,11 +15,54 @@ app = Celery(
 app.conf.beat_schedule = {
     "run-every-second": {
         "task": "tasks.send_telegram_message",
-        "schedule": 1.0,  # This sets the task to run every second
+        "schedule": schedule(60.0),
     },
 }
 
 
 @shared_task
 def send_telegram_message():
-    print(groups.col_count)
+    current_time = datetime.now()
+    today_day_number = current_time.weekday()
+    day_names = {
+        "Du": 0,
+        "Se": 1,
+        "Chor": 2,
+        "Pay": 3,
+        "Ju": 4,
+        "Shan": 5,
+        "Ya": 6,
+        "Har kuni": 99,
+    }
+
+    groups_dataframe = pandas.DataFrame(groups.get_all_records())
+    for _, group in groups_dataframe[
+        groups_dataframe["Status"] == "Boshlangan"
+    ].iterrows():
+        lesson_time = datetime.strptime(
+            str(group["Vaqti"]).split("-")[0],
+            "%H:%M",
+        )
+        earlier_time = (lesson_time - timedelta(minutes=30)).strftime("%H:%M")
+        lesson_days = str(group["Kunlari"]).split("-")
+        lesson_days_in_number = [day_names[day] for day in lesson_days]
+
+        if "Har kuni" in lesson_days:
+            invite_to_test(
+                current_time=current_time,
+                earlier_time=earlier_time,
+                group=group,
+                db=db,
+                bot=bot,
+            )
+            print("har kuni shu vaqtda jo'natilishi kerak")
+            print(earlier_time)
+
+        if today_day_number in lesson_days_in_number:
+            invite_to_test(
+                current_time=current_time,
+                earlier_time=earlier_time,
+                group=group,
+                db=db,
+                bot=bot,
+            )
