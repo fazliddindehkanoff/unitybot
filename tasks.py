cef8f00f70b9b2613a4cfedc80a81de1065cbf1f
FazliddinDehkanoff <fazliddindehkanoff@yandex.com>
2024-06-10 13:12:38 +0500

    bug fix

--- tasks.py
+++ tasks.py
@@ -1,4 +1,17 @@
-from celery import shared_task
+from celery import Celery, shared_task
+
+app = Celery(
+    "my_bot",
+    broker="redis://redis:6379/0",
+    backend="redis://redis:6379/0",
+)
+
+app.conf.beat_schedule = {
+    "run-every-second": {
+        "task": "tasks.send_telegram_message",
+        "schedule": 1.0,  # This sets the task to run every second
+    },
+}
 
 
 @shared_task
