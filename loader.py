2837d3336724b1c00c8944c684510fa98f34787c
FazliddinDehkanoff <fazliddindehkanoff@yandex.com>
2024-06-10 13:00:41 +0500

    bug fix

--- loader.py
+++ loader.py
@@ -3,9 +3,7 @@ import gspread
 
 from aiogram import Bot
 from aiogram.enums.parse_mode import ParseMode
-from celery.schedules import schedule
 
-from celery_config import app
 from utils.db.base import Database
 from data.config import BOT_TOKEN, DB_USER, DB_PASS, DB_NAME, DB_HOST
 
@@ -24,11 +22,3 @@ crm = sheet.worksheet("CRM")
 groups = sheet.worksheet("Guruhlar")
 tests = sheet.worksheet("Savollar")
 davomat = sheet.worksheet("Davomat")
-
-
-app.conf.beat_schedule = {
-    "send-daily-message": {
-        "task": "tasks.send_telegram_message",
-        "schedule": schedule(1.0),
-    },
-}
