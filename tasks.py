dc0e218e176fc6f71111682f2018c2a6c4f39017
FazliddinDehkanoff <fazliddindehkanoff@yandex.com>
2024-06-10 13:16:37 +0500

    bug fix

--- tasks.py
+++ tasks.py
@@ -1,4 +1,9 @@
+from datetime import datetime, timedelta
 from celery import Celery, shared_task
+import pandas
+
+from loader import bot, db, groups
+from utils.extra_datas import invite_to_test
 
 app = Celery(
     "my_bot",
@@ -15,5 +20,48 @@ app.conf.beat_schedule = {
 
 
 @shared_task
-def send_telegram_message():
-    print("TEST")
+async def notify_students():
+    current_time = datetime.now()
+    today_day_number = current_time.weekday()
+    day_names = {
+        "Du": 0,
+        "Se": 1,
+        "Chor": 2,
+        "Pay": 3,
+        "Ju": 4,
+        "Shan": 5,
+        "Ya": 6,
+        "Har kuni": 99,
+    }
+
+    groups_dataframe = pandas.DataFrame(groups.get_all_records())
+    for _, group in groups_dataframe[
+        groups_dataframe["Status"] == "Boshlangan"
+    ].iterrows():
+        lesson_time = datetime.strptime(
+            str(group["Vaqti"]).split("-")[0],
+            "%H:%M",
+        )
+        earlier_time = (lesson_time - timedelta(minutes=30)).strftime("%H:%M")
+        lesson_days = str(group["Kunlari"]).split("-")
+        lesson_days_in_number = [day_names[day] for day in lesson_days]
+
+        if "Har kuni" in lesson_days:
+            await invite_to_test(
+                current_time=current_time,
+                earlier_time=earlier_time,
+                group=group,
+                db=db,
+                bot=bot,
+            )
+            print("har kuni shu vaqtda jo'natilishi kerak")
+            print(earlier_time)
+
+        if today_day_number in lesson_days_in_number:
+            await invite_to_test(
+                current_time=current_time,
+                earlier_time=earlier_time,
+                group=group,
+                db=db,
+                bot=bot,
+            )
